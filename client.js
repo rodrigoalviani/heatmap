2646c99160c2793494fdc04b177b3b9c8e76d9d4
Rodrigo Alviani <rodrigoalviani@gmail.com>
2015-01-15 13:50:21 -0200

    Send client events to server url

--- client.js
+++ client.js
@@ -8,7 +8,7 @@ document.addEventListener('click', function next (e) {
   else
     xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
 
-  xmlhttp.open('POST', '', true);
+  xmlhttp.open('POST', 'http://localhost:3000/event', true);
   xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   xmlhttp.send('page=' + encodeURIComponent(document.URL) + '&x=' + e.pageX + '&y=' + e.pageY + '&w=' + window.innerWidth + '&h=' + window.innerHeight);
 });
