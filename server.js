a0a2004fa86dd48a0e3dbc99637a19c7360f5fe4
Rodrigo Alviani <rodrigoalviani@gmail.com>
2015-02-13 10:51:47 -0200

    Fix cars use

--- server.js
+++ server.js
@@ -3,14 +3,15 @@
 var express = require('express')
   , bodyParser = require('body-parser')
   , mongoose = require('mongoose')
+  , cors = require('./cors')
   , app = express();
 
-require('./cors');
 require('./models/click');
 var Click = mongoose.model('Click');
 mongoose.connect('mongodb://localhost/heatmap');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cors);
 
 app.post('/event', function (req, res) {
   var click = new Click(req.body);
